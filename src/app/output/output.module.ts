997cb04fff3c01d98aaa12897cd6c9f6d9a0188f
johnj974 <jjdug@hotmail.com>
2021-10-01 15:55:22 +0100

    main routing modules added

---
+++ src/app/output/output.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OutputRoutingModule } from './output-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OutputRoutingModule
+  ]
+})
+export class OutputModule { }
